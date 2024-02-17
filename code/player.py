e98c35732290160d6a267593e678953e259d6dda
Maksim Kudaev <kudaev.m@promail366.com>
2024-02-18 00:06:20 +0300

    feat: add final player animations

--- code/player.py
+++ code/player.py
@@ -1,5 +1,7 @@
 import pygame
 
+from os import walk
+
 
 class Player(pygame.sprite.Sprite):
     
@@ -8,8 +10,10 @@ class Player(pygame.sprite.Sprite):
         
         # image
         self.import_assets()
+        
         self.frame_index = 0
-        self.image = self.animation[self.frame_index]
+        self.state = 'down'
+        self.image = self.animations[self.state][self.frame_index]
         self.rect = self.image.get_rect(center=pos)
         
         # float based movement
@@ -18,8 +22,17 @@ class Player(pygame.sprite.Sprite):
         self.speed = 250
     
     def import_assets(self):
-        path = 'graphics/player/right'
-        self.animation = [pygame.image.load(file=f'{path}/{frame}.png').convert_alpha() for frame in range(4)]
+        self.animations = {}
+        for index, folder in enumerate(walk('graphics/player')):
+            if index == 0:
+                for folder_name in folder[1]:
+                    self.animations[folder_name] = []
+            else:
+                for file_name in folder[2]:
+                    path = folder[0].replace('\\', '/') + '/' + file_name
+                    surf = pygame.image.load(file=path).convert_alpha()
+                    key = folder[0].split('\\')[1]
+                    self.animations[key].append(surf)
     
     def move(self, dt):
         if self.direction.magnitude():
@@ -33,27 +46,35 @@ class Player(pygame.sprite.Sprite):
         
         # horizontal movement
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.state = 'right'
             self.direction.x = 1
         elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.state = 'left'
             self.direction.x = -1
         else:
             self.direction.x = 0
 
         # vertical movement
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.state = 'down'
             self.direction.y = 1
         elif keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.state = 'up'
             self.direction.y = -1
         else:
             self.direction.y = 0
     
     def animate(self, dt):
-        self.frame_index += 8 * dt
+        current_animation = self.animations[self.state]
         
-        if self.frame_index >= len(self.animation):
+        if self.direction.magnitude():
+            self.frame_index += 8 * dt
+            if self.frame_index >= len(current_animation):
+                self.frame_index = 0
+        else:
             self.frame_index = 0
         
-        self.image = self.animation[int(self.frame_index)]
+        self.image = current_animation[int(self.frame_index)]
     
     def update(self, dt):
         self.input()
