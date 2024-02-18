d32c09e5e1b47ed7bebff4793c2a3257b4210aab
Maksim Kudaev <kudaev.m@promail366.com>
2024-02-18 18:49:41 +0300

    feat: add basic cars and float based movement

---
+++ code/car.py
@@ -0,0 +1,34 @@
+import pygame
+
+from random import choice
+from os import walk
+
+
+class Car(pygame.sprite.Sprite):
+    
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        
+        # image
+        car_name = choice(list(walk('graphics/cars'))[0][2])
+        self.image = pygame.image.load(file=f'graphics/cars/{car_name}').convert_alpha()
+        self.rect = self.image.get_rect(center=pos)
+        
+        # float based movement
+        self.pos = pygame.math.Vector2(self.rect.center)
+        
+        if self.pos.x < 200:
+            self.direction = pygame.math.Vector2(1, 0)
+        else:
+            self.direction = pygame.math.Vector2(-1, 0)
+            self.image = pygame.transform.flip(surface=self.image, flip_x=True, flip_y=False)
+            
+        self.speed = 300
+    
+    def update(self, dt):
+        self.pos += self.direction * self.speed * dt
+        self.rect.center = (round(self.pos.x), round(self.pos.y))
+        
+        if not -200 < self.rect.x < 3400:
+            self.kill()
+        
--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ import pygame, sys
 
 from settings import WINDOW_WIDTH, WINDOW_HEIGHT
 from player import Player
+from car import Car
 
 # game setup
 pygame.init()
@@ -14,6 +15,7 @@ all_sprites = pygame.sprite.Group()
 
 # sprites
 player = Player(pos=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), groups=all_sprites)
+car = Car(pos=(600, 200), groups=all_sprites)
 
 # game loop
 while True:
